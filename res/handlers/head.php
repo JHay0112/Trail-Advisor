458dc13af80532f2c1dbfde3fe89e6149f1c6878
JHay0112 <jordanhay0112@gmail.com>
2020-04-23 10:23:59 +1200

    Page attr documentation

--- res/handlers/head.php
+++ res/handlers/head.php
@@ -17,7 +17,23 @@
 
     */
 
-    // Default page attributes, if a page attribute is not specified in the parent file it will be decided by these defaults
+    /* 
+    
+        Default page attributes, if a page attribute is not specified in the parent file it will be decided by these defaults
+
+        title: Page title
+        site_name: Name of the site
+        author: Who wrote the code in the page (used in meta data as well)
+        path: Path to the page as shown in url
+        desc: Page description (for meta data)
+        keywords: Keywords for meta data (SEO)
+        stylesheeets: Stylesheets used in the page
+        scripts: JavaScript resources to be loaded in the page
+        favicon: Page favicon
+        permitted_users: Types of users who are allowed on the page
+
+    */
+    
     $def_page_attr = array(
         "title" => "Unknown Page",
         "site_name" => "TrailAdvisor",
@@ -28,8 +44,7 @@
         "stylesheets" => array("css/styles.css", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"),
         "scripts" => array("js/main.js"),
         "favicon" => "img/favicon.png",
-        "authority_levels" => array("Standard", "Staff", "Administrator");
-        "minimum_authority" => "";
+        "permitted_users" => array("Admin", "Staff", "Standard", "");
     );
 
     $page_attr = array_merge($def_page_attr, $page_attr); // Fill page_attr with default values if they have not been set in page_attr
