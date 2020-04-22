e3f3c439dbb3fd93e7d839c3e69a066f055b6c9e
JHay0112 <jordanhay0112@gmail.com>
2020-04-23 10:43:28 +1200

    head and foot in wrong folder

--- res/head.php
+++ res/head.php
@@ -33,7 +33,7 @@
         permitted_users: Types of users who are allowed on the page
 
     */
-    
+
     $def_page_attr = array(
         "title" => "Unknown Page",
         "site_name" => "TrailAdvisor",
@@ -44,7 +44,7 @@
         "stylesheets" => array("css/styles.css", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"),
         "scripts" => array("js/main.js"),
         "favicon" => "img/favicon.png",
-        "permitted_users" => array("Admin", "Staff", "Standard", "");
+        "permitted_users" => array("Admin", "Staff", "Standard", "")
     );
 
     $page_attr = array_merge($def_page_attr, $page_attr); // Fill page_attr with default values if they have not been set in page_attr
