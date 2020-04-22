86be3e4dfa6b71f0a823523e3383ac9cee22eec6
JHay0112 <jordanhay0112@gmail.com>
2020-04-23 10:46:57 +1200

    Page data generation from page_attr

--- res/head.php
+++ res/head.php
@@ -40,7 +40,7 @@
         "author" => "Unknown",
         "path" => "Unknown",
         "desc" => "No Description",
-        "keywords" => "",
+        "keywords" => "Trails, Find, Create, Edit, Make, Like, Tracks, Walks, Hiking",
         "stylesheets" => array("css/styles.css", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"),
         "scripts" => array("js/main.js"),
         "favicon" => "img/favicon.png",
@@ -50,3 +50,34 @@
     $page_attr = array_merge($def_page_attr, $page_attr); // Fill page_attr with default values if they have not been set in page_attr
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- Meta Data -->
+        <meta charset="UTF-8" />
+        <meta name="keywords" content="<?php print($page_attr["keywords"]) ?>" />
+        <meta name="author" content="<?php print($page_attr["author"]) ?>" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <!-- Stylesheet(s) -->
+        <?php 
+            // For every stylesheet in the page attr it will create a new html link for it.
+            for($i = 0; $i < sizeof($page_attr["stylesheets"]); $i++) {
+                print('<link rel="stylesheet" type="text/css" href="'.$page_attr["stylesheets"][$i].'" />');
+            } 
+        ?>
+
+        <!-- Script(s) -->
+        <?php 
+            // For every script in the page attr it will create a new html script tage for it.
+            for($i = 0; $i < sizeof($page_attr["scripts"]); $i++) {
+                print('<script src="'.$page_attr["scripts"][$i].'"></script>');
+            }
+        ?>
+
+        <!-- Tab Data -->
+        <link rel="icon" href="<?php print($page_attr["favicon"]) ?>" />
+        <title><?php print($page_attr["title"]." - ".$page_attr["site_name"]); ?></title>
+    </head>
+    <body>
