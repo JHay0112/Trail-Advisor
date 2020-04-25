1796c45a395234ca849ce308f248f863ca9608b0
JHay0112 <jordanhay0112@gmail.com>
2020-04-25 21:24:17 +1200

    Add connect file

---
+++ res/connect.php
@@ -0,0 +1,27 @@
+<?php
+
+    /*
+
+        res/connect.php
+        
+        Author: Jordan Hay
+        Version: 1.0
+
+        Description:
+
+            Handles connection to database
+
+        Other Notes:
+
+            N/a
+
+    */
+
+    $link = mysqli_connect(
+        "localhost", 
+        "root", 
+        "", 
+        "trailadvisor") 
+    or  die("Couldn't connect to DB server.");
+
+?>
