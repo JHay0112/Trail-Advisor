9863b93fe7e32b17cfc0e759fdcccd90514ff9a8
JHay0112 <jordanhay0112@gmail.com>
2020-04-26 17:14:23 +1200

    Add logout function

---
+++ res/handlers/logoutuser.php
@@ -0,0 +1,32 @@
+<?php
+
+    /*
+
+        res/handlers/logoutuser.php
+        
+        Author: Jordan Hay
+        Version: 1.0
+
+        Description:
+
+            Logs current user out.
+
+        Other Notes:
+
+            N/a
+
+    */
+
+    // Remove the session variables, this logs out the user.
+    session_start();
+    session_unset();
+
+    // Now alert user they have been logged out.
+    print("
+        <script>
+            alert('Logged out. Redirecting to home page.');
+            location = '../../index.php';
+        </script>
+    ");
+
+?>
