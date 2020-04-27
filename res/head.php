b000a6d2f1aae851b7ab79b9262a02f683fcd7eb
JHay0112 <jordanhay0112@gmail.com>
2020-04-27 14:20:06 +1200

    Change absolute path to relative

--- res/head.php
+++ res/head.php
@@ -115,10 +115,10 @@
         // If user does not have required permissions, alert the user to the issue and redirect to login page. In case that script fails, although I don't think it could, stop all code execution with the exit statement
         if($logged_in) {
             // If the user is logged in send them to their profile
-            print("<script>location = '/profile.php?referall_case=useroutofbounds';</script>");
+            print("<script>location = 'profile.php?referall_case=useroutofbounds';</script>");
         } else {
             // If user is not logged in send them to login
-            print("<script>location = '/login.php?referral_case=useroutofbounds';</script>");
+            print("<script>location = 'login.php?referral_case=useroutofbounds';</script>");
         }
         exit();
     }
