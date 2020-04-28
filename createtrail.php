b5a86da8d927815d9e5f9c677fb044b6734ac177
JHay0112 <jordanhay0112@gmail.com>
2020-04-28 15:15:55 +1200

    Select location on map in create trail

--- createtrail.php
+++ createtrail.php
@@ -21,7 +21,9 @@
     $page_attr = array(
         "title" => "Create Trail",
         "author" => "Jordan Hay",
-        "permitted_users" => array("Admin", "Staff")
+        "permitted_users" => array("Admin", "Staff"),
+        "stylesheets" => array("css/styles.css", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css", "http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css"),
+        "scripts" => array("js/lib/jquery.slim.min.js", "js/lib/parsley.min.js", "js/main.js", "http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js"),
     );
 
     require_once("res/head.php");
@@ -70,26 +72,60 @@
 
     <fieldset class="col-12">
 
-        <legend>Trail Location: </legend>
+        <legend>Select Trail Location: </legend>
 
-        <div class="row">
+        <div class="col-12">
 
-            <div class="col-6 form-wrapper">
+            <div id="map" name="map" class="col-12" style="height: 500px;"></div>
 
-                <label class="col-12" for="lat">Latitude:</label>
-                <input type="number" name="lat" min="0" class="col-12" placeholder="Latitude" required />
+            <input id="lat" type="number" readonly name="lat" min="-90" max="90" class="col-6" placeholder="Latitude" required />
+            <input id="lng" type="number" readonly name="lng" min="-180" max="180" class="col-6" placeholder="Longitude" required />
 
-                <label class="col-12" for="lng">Longitude:</label>
-                <input type="number" name="lng" min="0" class="col-12" placeholder="Longitude" required />
+            <script>
 
-            </div>
+                // Script for leaflet map to update from coords and vice versa
+                // Adapted from: https://gist.github.com/answerquest/03ade545b071b3e5ea4e
 
-            <div class="col-6 form-wrapper">
+                var tileLayer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> Contributors'
+                });
 
-                <label class="col-12" for="map">Select on map:</label>
-                <!-- Map -->
-            
-            </div>
+                //remember last position
+                var rememberLat = document.getElementById('lat').value;
+                var rememberLong = document.getElementById('lng').value;
+                if(!rememberLat || !rememberLong) {rememberLat = -43.53; rememberLong = 172.63;}
+
+                var map = new L.Map('map', {
+                    'center': [rememberLat, rememberLong],
+                    'zoom': 12,
+                    'layers': [tileLayer]
+                });
+
+                var marker = L.marker([rememberLat, rememberLong],{
+                draggable: true
+                }).addTo(map);
+
+                marker.on('dragend', function (e) {
+                    updateLatLng(marker.getLatLng().lat, marker.getLatLng().lng);
+                });
+
+                map.on('click', function (e) {
+                    marker.setLatLng(e.latlng);
+                    updateLatLng(marker.getLatLng().lat, marker.getLatLng().lng);
+                });
+
+                function updateLatLng(lat, lng, reverse) {
+                    if(reverse) {
+                        marker.setLatLng([lat,lng]);
+                        map.panTo([lat,lng]);
+                    } else {
+                    document.getElementById('lat').value = marker.getLatLng().lat;
+                    document.getElementById('lng').value = marker.getLatLng().lng;
+                    map.panTo([lat,lng]);
+                    }
+                }
+
+            </script>
         
         </div>
 
