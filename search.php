0e611b042aafb6ebd52530dab1e46a3acd2956bc
JHay0112 <jordanhay0112@gmail.com>
2020-05-01 11:04:45 +1200

    Navigation for search results

--- search.php
+++ search.php
@@ -67,14 +67,19 @@
         <label for="map">Select location on map to find nearby trails:</label>
         <div id="trail-map" name="map" class="col-12" style="height: 500px;"></div>
 
-        <div class="col-6 form-wrapper">
+        <div class="col-4 form-wrapper">
             <label for="lat" class="col-12">Latitude:</label>
             <input id="lat" type="number" name="lat" min="-90" max="90" step="any" class="col-12" placeholder="Latitude" value="<?php print($lat) ?>" required readonly />
         </div>
 
-        <div class="col-6 form-wrapper">
+        <div class="col-4 form-wrapper">
             <label for="lng" class="col-12">Longitude:</label>
             <input id="lng" type="number" name="lng" min="-180" max="180" step="any" class="col-12" placeholder="Longitude" value="<?php print($lng) ?>" required readonly />
+        </div>
+
+        <div class="col-4 form-wrapper">
+            <label for="trails_to_load" class="col-12">Trails per page:</label>
+            <input id="trails_to_load" type="number" name="rows_to_load" min="20" max="100" step="1" class="col-12" placeholder="Trails To Load" value="<?php print($rows_to_load) ?>" required />
         </div>
 
         <input type="submit" value="Search" class="col-12" />
@@ -89,6 +94,8 @@
     // Generating bounds for query
     $lower_limit = $page * $rows_to_load;
     $upper_limit = ($page + 1) * $rows_to_load;
+    // Getting the total amount of trails in trails table, used for arrows at page bottom
+    $total_rows = mysqli_fetch_assoc(mysqli_query($link, "SELECT COUNT(trail_id) AS count FROM trails"))["count"];
         
     // This is one hell of a prepared statment to try and explain
     $stmt = mysqli_prepare($link, "SELECT trails.trail_id, trails.name AS trail_name, users.username AS creator, COUNT(trail_likes.trail_id) AS likes FROM trails INNER JOIN users ON trails.creator = users.user_id LEFT JOIN trail_likes ON trails.trail_id = trail_likes.trail_id GROUP BY trails.trail_id ORDER BY (ABS(trails.lat - ?) + ABS(trails.lng - ?)) ASC LIMIT ?, ?");
@@ -122,6 +129,21 @@
     } else {
         print("<p>No Results</p>");
     }
+
+    // Generating buttons for the user to click to navigate results
+    print("<div class='col-12' id='search-controls'>");
+
+    // If the page is greater than zero allow to navigate back
+    if($page > 0) {
+        print("<a class=\"navigate-result-buttons\" id=\"previous-page-button\" href=\"search.php?page=".($page - 1)."&load=".$rows_to_load."&lat=".$lat."&lng=".$lng."#search-results\"><span class=\"fa fa-arrow-left\"></span> Previous Page</a>");
+    }
+
+    // If the amount of rows to load on the next page does not exceed the total rows in the trail then allow the user to access the next page
+    if((($rows_to_load * ($page + 1)) < $total_rows)) {
+        print("<a class=\"navigate-result-buttons\" id=\"next-page-button\" href=\"search.php?page=".($page + 1)."&load=".$rows_to_load."&lat=".$lat."&lng=".$lng."#search-results\">NextPage <span class=\"fa fa-arrow-right\"></span></a>");
+    }
+
+    print("</div>");
 
     require_once("res/foot.php");
 
