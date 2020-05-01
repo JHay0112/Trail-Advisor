c459d6b4f32519d12fc4c940818e48e7d230cad6
JHay0112 <jordanhay0112@gmail.com>
2020-05-01 13:40:18 +1200

    Edit trails

---
+++ edittrail.php
@@ -0,0 +1,129 @@
+<?php
+
+    /*
+
+        edittrail.php
+        
+        Author: Jordan Hay
+        Version: 1.0
+
+        Description:
+
+            Allow admin and staff users to edit trails.
+
+        Other Notes:
+
+            N/a
+
+    */
+
+    require_once("res/initsession.php");
+    require_once("res/connect.php");
+    require_once("res/checkfields.php");
+
+    // If these fields aren't there send back to search page
+    check_fields($_GET, array("trail"), "../../search.php");
+
+    $error = false;
+
+    // Cast to int
+    $trail_id = (int)$_GET["trail"];
+
+    if($trail_id != 0) {
+        $stmt = mysqli_prepare($link, "SELECT name, description, lat, lng FROM trails WHERE trail_id = ?");
+
+        // Check stmt formed
+        if($stmt) {
+            mysqli_stmt_bind_param($stmt, "i", $trail_id);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_bind_result($stmt, $trail_name, $trail_description, $lat, $lng);
+            mysqli_stmt_fetch($stmt);
+        } else {
+            $error = true;
+        }
+    } else {
+       $error = true;
+    }
+
+    if($trail_name == "") {
+        $error = true;
+    }
+
+    // Error handling 
+    if($error) {
+        $trail_name = "Unknown Trail";
+        $trail_description = "This trail does not exist!";
+        $lat = 0;
+        $lng = 0;
+    }
+
+    // Page Attributes
+    $page_attr = array(
+        "title" => "Edit Trail",
+        "author" => "Jordan Hay",
+        "permitted_users" => array("Admin", "Staff"),
+        "onload" => "genTrailMap(zoom = 12, select = true);"
+    );
+
+    // Add page to nav
+    $nav = array($page_attr["title"] => array("href" => "edittrail.php?trail=".$trail_id, "classes" => ""));
+
+    require_once("res/head.php");
+
+    require_once("res/referralcase.php");
+
+    $states = array(
+        "edittrailfail" => "Failed to edit trail, please try again.",
+        "missingfields" => "Not all required fields were filled."
+    );
+
+    if(isset($_GET["referral_case"])) {
+        print("<p>".referral($_GET["referral_case"], $states)."</p>");
+    }
+
+?>
+
+<form class="col-12 form-wrapper" data-parsley-validate action="res/handlers/updatetrail.php" method="post" enctype="multipart/form-data">
+
+    <fieldset class="col-12">
+
+        <legend>Edit General Trail Information:</legend>
+
+        <label class="col-12" for="name">Trail Name:</label>
+        <input type="text" name="name" maxlength="30" class="col-12" placeholder="Trail Name" value="<?php print($trail_name); ?>" required />
+
+        <label class="col-12" for="description">Trail Description:</label>
+        <textarea type="text" name="description" class="col-12" placeholder="Trail Description" required><?php print($trail_description); ?></textarea>
+
+    </fieldset>
+
+    <fieldset class="col-12">
+
+        <legend>Edit Trail Location: </legend>
+
+        <div class="col-12">
+
+            <div id="trail-map" name="map" class="col-12" style="height: 500px;"></div>
+
+            <div class="col-6 form-wrapper">
+                <label for="lat" class="col-12">Latitude:</label>
+                <input id="lat" type="number" name="lat" min="-90" max="90" step="any" class="col-12" placeholder="Latitude" value="<?php print($lat); ?>" required readonly />
+            </div>
+
+            <div class="col-6 form-wrapper">
+                <label for="lng" class="col-12">Longitude:</label>
+                <input id="lng" type="number" name="lng" min="-180" max="180" step="any" class="col-12" placeholder="Longitude" value="<?php print($lng); ?>" required readonly />
+            </div>
+        
+        </div>
+
+    </fieldset>
+
+    <input type="hidden" name="trail_id" value="<?php print($trail_id); ?>" />
+    <input type="hidden" name="token" value="<?php print($token); ?>" />
+
+    <input type="submit" value="Edit Trail" class="col-12" <?php if($error) {print("disabled");} // If an error has occured disable this button ?> />
+
+</form>
+
+<?php require_once("res/foot.php"); ?>
---
+++ res/handlers/updatetrail.php
@@ -0,0 +1,75 @@
+<?php
+
+    /*
+
+        res/handlers/updatetrail.php
+        
+        Author: Jordan Hay
+        Version: 1.0
+
+        Description:
+
+            Updates an existing trails info.
+
+        Other Notes:
+
+            N/a
+
+    */
+
+    $token_required = true;
+    require_once("../initsession.php");
+    require_once("../connect.php");
+    require_once("../checkfields.php");
+
+    // Check all required fields exist
+    check_fields($_POST, array("trail_id", "name", "description", "lat", "lng"), "../../edittrail.php");
+
+    $trail_id = (int)$_POST["trail_id"];
+    $name = strip_tags($_POST["name"]);
+    $description = strip_tags($_POST["description"]);
+    $lat = (double)$_POST["lat"];
+    $lng = (double)$_POST["lng"];
+
+    // If trail id is invalid send them back to edit dialogue
+    if($trail_id == 0) {
+        print("<script> location = '../../editrail.php?trail=".$trail_id."'</script>");
+    }
+
+    // Prepare statment
+    $stmt = mysqli_prepare($link, "UPDATE trails SET name = ?, description = ?, lat = ?, lng = ? WHERE trail_id = ?");
+
+    // Binding parameters and executing statement, checking if $stmt has formed first
+    if($stmt) {
+        mysqli_stmt_bind_param($stmt, "ssddi", $name, $description, $lat, $lng, $trail_id);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_close($stmt);
+    }
+
+    // Prepare new statment, this looks for the user being an editor or creator of the trail in question
+    $stmt = mysqli_prepare($link, "SELECT trail_id FROM trail_editors WHERE trail_id = ? AND user_id = ?");
+
+    // Binding parameters and executing statement, checking if $stmt has formed first
+    if($stmt) {
+        mysqli_stmt_bind_param($stmt, "ii", $trail_id, $user_info["user_id"]);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_store_result($stmt);
+    }
+
+    // if no rows returned user has not edited this trail before so add them to editors list
+    if(mysqli_stmt_num_rows($stmt) == 0) {
+        mysqli_stmt_close($stmt);
+
+        // Prepare new statment to add new editor to trail
+        $stmt = mysqli_prepare($link, "INSERT INTO trail_editors (trail_id, user_id) VALUES (?, ?)");
+
+        // Binding parameters and executing statement, checking if $stmt has formed first
+        if($stmt) {
+            mysqli_stmt_bind_param($stmt, "ii", $trail_id, $user_info["user_id"]);
+            mysqli_stmt_execute($stmt);
+        }
+    }
+
+    print("<script>location = '../../trail.php?trail=".$trail_id."'</script>");
+
+?>
