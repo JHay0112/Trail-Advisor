6a3fcf0ff6a884fdf29e2105cabde752106f7fd6
JHay0112 <jordanhay0112@gmail.com>
2020-05-05 10:34:41 +1200

    Place holder index text

--- index.php
+++ index.php
@@ -27,6 +27,6 @@
 
 ?>
 
-
+<p>I plan to write some super-businessy like here. Just imagine it for the time being. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sollicitudin vehicula ex sit amet vehicula. Nulla egestas accumsan turpis, in luctus risus pretium sit amet. Aliquam ipsum ante, ultrices vel cursus eu, tincidunt vel magna. Praesent arcu sem, semper et ante et, suscipit venenatis ligula. In at risus id libero aliquet consequat eget vitae sapien. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed pharetra elit non neque posuere, vel interdum elit tincidunt. Curabitur massa neque, semper sed augue quis, commodo ultrices sapien. Fusce rhoncus condimentum eros ac imperdiet. Aliquam tempor a ipsum ut molestie. In aliquam nibh eu iaculis consectetur. Vivamus eget ante nec nulla semper pulvinar. Vivamus porttitor bibendum elementum. Cras gravida eleifend erat nec pretium. Integer tincidunt sapien tortor, eu imperdiet felis ornare eget.</p>
 
 <?php require_once("res/foot.php"); ?>
