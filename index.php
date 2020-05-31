c0bf6a188006441a2c98a5325e74e1df4cbd6b45
JHay0112 <jordanhay0112@gmail.com>
2020-06-01 10:23:02 +1200

    index content

--- index.php
+++ index.php
@@ -20,13 +20,26 @@
     // Page Attributes
     $page_attr = array(
         "title" => "Home",
-        "author" => "Jordan Hay"
+        "author" => "Jordan Hay",
+        "class" => "home"
     );
 
     require_once("res/head.php");
 
 ?>
 
-<p>I plan to write some super-businessy like here. Just imagine it for the time being. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sollicitudin vehicula ex sit amet vehicula. Nulla egestas accumsan turpis, in luctus risus pretium sit amet. Aliquam ipsum ante, ultrices vel cursus eu, tincidunt vel magna. Praesent arcu sem, semper et ante et, suscipit venenatis ligula. In at risus id libero aliquet consequat eget vitae sapien. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed pharetra elit non neque posuere, vel interdum elit tincidunt. Curabitur massa neque, semper sed augue quis, commodo ultrices sapien. Fusce rhoncus condimentum eros ac imperdiet. Aliquam tempor a ipsum ut molestie. In aliquam nibh eu iaculis consectetur. Vivamus eget ante nec nulla semper pulvinar. Vivamus porttitor bibendum elementum. Cras gravida eleifend erat nec pretium. Integer tincidunt sapien tortor, eu imperdiet felis ornare eget.</p>
+<h1>Welcome to <?php print($page_attr["site_name"]); ?>!</h1>
+
+<hr />
+
+<p><?php print($page_attr["site_name"]); ?> is a website dedicated to the creation and sharing of trails for all people of all abilities. Whether you're a sunday stroller or a dedicated hiker <?php print($page_attr["site_name"]); ?> has trails for you to discover and enjoy. </p>
+
+<p>Ready to get walking? You can search our database of trails <a href="search.php">here</a>.</p>
+
+<hr />
+
+<p>The details of trails are provided by our faithful users and moderated by our dedicated staff team.</p>
+
+<p>Interested in contributing to our ever-growing database of trails? Just sign up <a href="signup.php">here</a>.</p>
 
 <?php require_once("res/foot.php"); ?>
